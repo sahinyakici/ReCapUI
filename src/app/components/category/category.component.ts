9897840e329aad8a81e13b097b0927b2e3dc11d4
Şahin Yakıcı <shnykc@hotmail.com>
2023-03-30 02:04:11 +0300

    I watched "18. Gün Angular Giriş 3".

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Category} from "../../models/category";
+import {CategoryService} from "../../services/category.service";
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
+  categories: Category[] = [];
+  currentCategory: Category | null;
 
+  constructor(private categoryService: CategoryService) {
+  }
+
+  ngOnInit(): void {
+    this.getCategories()
+  }
+
+  getCategories() {
+    this.categoryService.getCategories().subscribe((result) => {
+      this.categories = result.data
+    })
+  }
+
+  setCurrentCategory(category: Category | null) {
+    this.currentCategory = category;
+  }
+
+  getCurrentCategoryClass(category: Category) {
+    if (this.currentCategory == category) {
+      return "list-group-item active"
+    } else {
+      return "list-group-item"
+    }
+  }
+
+  getAllCategories() {
+    if (!this.currentCategory) {
+      return "list-group-item active"
+    } else {
+      return "list-group-item"
+    }
+  }
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import {Product} from "../../models/product";
 import {ProductService} from "../../services/product.service";
 
@@ -9,19 +10,32 @@ import {ProductService} from "../../services/product.service";
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [];
-  dataLoaded:boolean=false;
+  dataLoaded: boolean = false;
 
-  constructor(private productService: ProductService) {
+  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit(): void {
-    this.getProducts();
+    this.activatedRoute.params.subscribe(params => {
+      if (params["categoryId"]) {
+        this.getProductsByCategory(params["categoryId"])
+      } else {
+        this.getProducts();
+      }
+    })
   }
 
   getProducts() {
     this.productService.getProducts().subscribe(response => {
       this.products = response.data
-      this.dataLoaded=true;
+      this.dataLoaded = true;
+    })
+  }
+
+  getProductsByCategory(categoryId: number) {
+    this.productService.getProductsByCategory(categoryId).subscribe(response => {
+      this.products = response.data
+      this.dataLoaded = true;
     })
   }
 }
